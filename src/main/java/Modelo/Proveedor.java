f365ac79984d158dfef9ffbf832ca2e5d080acd6
Andrle <andrelageu0607@gmail.com>
2024-10-23 20:25:30 -0500

    Se agrega el modelo del proveedor

---
+++ src/main/java/Modelo/Proveedor.java
@@ -0,0 +1,61 @@
+package Modelo;
+
+public class Proveedor {
+    private int idProveedor;
+    private String nombre;
+    private String telefono;
+    private String correo;
+    private String dirección;
+
+    public Proveedor() {
+    }
+
+    public Proveedor(String nombre, String telefono, String correo, String dirección) {
+        this.nombre = nombre;
+        this.telefono = telefono;
+        this.correo = correo;
+        this.dirección = dirección;
+    }
+
+    public int getIdProveedor() {
+        return idProveedor;
+    }
+
+    public void setIdProveedor(int idProveedor) {
+        this.idProveedor = idProveedor;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public String getTelefono() {
+        return telefono;
+    }
+
+    public void setTelefono(String telefono) {
+        this.telefono = telefono;
+    }
+
+    public String getCorreo() {
+        return correo;
+    }
+
+    public void setCorreo(String correo) {
+        this.correo = correo;
+    }
+
+    public String getDirección() {
+        return dirección;
+    }
+
+    public void setDirección(String dirección) {
+        this.dirección = dirección;
+    }
+    
+    
+}
