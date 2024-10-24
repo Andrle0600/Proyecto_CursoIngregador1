741a205e1ad5c76d328323fbab845b446b541b88
Andrle <andrelageu0607@gmail.com>
2024-10-23 21:00:27 -0500

    Todo el DAO para proveedor

---
+++ src/main/java/DAO/ProveedorDAO.java
@@ -0,0 +1,13 @@
+package DAO;
+
+import Modelo.Proveedor;
+
+public interface ProveedorDAO {
+    void crear(Proveedor prov);
+
+    Proveedor leer(int id);
+
+    void actualizar(Proveedor prov);
+
+    void eliminar(int id);
+}
