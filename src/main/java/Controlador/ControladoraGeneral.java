7f457f6aff10a93ea2807a5fb71b4b191a6ad89f
Andrle <andrelageu0607@gmail.com>
2024-11-12 17:38:46 -0500

    Agregado controladoras de producto y detallePedido

--- src/main/java/Controlador/ControladoraGeneral.java
+++ src/main/java/Controlador/ControladoraGeneral.java
@@ -4,18 +4,30 @@ public class ControladoraGeneral {
 
     private ControladoraProveedor controlProveedor;
     private ControladoraPedido controlPedido;
+    private ControladoraProducto controlProducto;
+    private ControladoraDetallePedido controlDetalle;
 
     public ControladoraGeneral() {
         this.controlProveedor = new ControladoraProveedor();
         this.controlPedido = new ControladoraPedido();
+        this.controlProducto = new ControladoraProducto();
+        this.controlDetalle = new ControladoraDetallePedido();
     }
 
     // Métodos para obtener las controladoras específicas
     public ControladoraProveedor getControladoraProveedor() {
         return controlProveedor;
     }
-    
-    public ControladoraPedido getControladoraPedido(){
+
+    public ControladoraPedido getControladoraPedido() {
         return controlPedido;
+    }
+
+    public ControladoraProducto getControladoraProducto() {
+        return controlProducto;
+    }
+    
+    public ControladoraDetallePedido getControladoraDetallePedido(){
+        return controlDetalle;
     }
 }
