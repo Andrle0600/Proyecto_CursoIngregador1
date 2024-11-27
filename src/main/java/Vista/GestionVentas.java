49f11b6a7cbd9783ad4f9a53e87083c30963fd7a
Andrle <andrelageu0607@gmail.com>
2024-11-27 14:45:02 -0500

    Corrección estética de gestión de ventas

--- src/main/java/Vista/GestionVentas.java
+++ src/main/java/Vista/GestionVentas.java
@@ -15,9 +15,7 @@ public class GestionVentas extends javax.swing.JFrame {
         jPanel1 = new javax.swing.JPanel();
         jLabel1 = new javax.swing.JLabel();
         btnVenta = new javax.swing.JButton();
-        btnNotaVenta = new javax.swing.JButton();
         btnVentasUltimoMes = new javax.swing.JButton();
-        btnVentasUltimoAño = new javax.swing.JButton();
         btnVolver = new javax.swing.JButton();
 
         setDefaultCloseOperation(javax.swing.WindowConstants.EXIT_ON_CLOSE);
@@ -27,11 +25,13 @@ public class GestionVentas extends javax.swing.JFrame {
         jPanel1.setBackground(new java.awt.Color(182, 127, 226));
         jPanel1.setForeground(new java.awt.Color(182, 127, 226));
 
-        jLabel1.setFont(new java.awt.Font("Segoe UI", 1, 12)); // NOI18N
+        jLabel1.setFont(new java.awt.Font("Segoe UI", 1, 24)); // NOI18N
         jLabel1.setForeground(new java.awt.Color(0, 0, 0));
-        jLabel1.setText("REGISTRO DE NOTAS Y TRANSACCIONES DE VENTAS");
+        jLabel1.setText("GESTIÓN DE VENTAS");
 
         btnVenta.setBackground(new java.awt.Color(200, 76, 229));
+        btnVenta.setFont(new java.awt.Font("Segoe UI", 1, 14)); // NOI18N
+        btnVenta.setForeground(new java.awt.Color(0, 0, 0));
         btnVenta.setText("VENTA");
         btnVenta.addActionListener(new java.awt.event.ActionListener() {
             public void actionPerformed(java.awt.event.ActionEvent evt) {
@@ -39,27 +39,13 @@ public class GestionVentas extends javax.swing.JFrame {
             }
         });
 
-        btnNotaVenta.setBackground(new java.awt.Color(200, 76, 229));
-        btnNotaVenta.setText("NOTA DE VENTA");
-        btnNotaVenta.addActionListener(new java.awt.event.ActionListener() {
-            public void actionPerformed(java.awt.event.ActionEvent evt) {
-                btnNotaVentaActionPerformed(evt);
-            }
-        });
-
         btnVentasUltimoMes.setBackground(new java.awt.Color(200, 76, 229));
-        btnVentasUltimoMes.setText("VENTAS DEL ULTIMO MES");
+        btnVentasUltimoMes.setFont(new java.awt.Font("Segoe UI", 1, 14)); // NOI18N
+        btnVentasUltimoMes.setForeground(new java.awt.Color(0, 0, 0));
+        btnVentasUltimoMes.setText("REPORTE DE VENTAS");
         btnVentasUltimoMes.addActionListener(new java.awt.event.ActionListener() {
             public void actionPerformed(java.awt.event.ActionEvent evt) {
                 btnVentasUltimoMesActionPerformed(evt);
-            }
-        });
-
-        btnVentasUltimoAño.setBackground(new java.awt.Color(200, 76, 229));
-        btnVentasUltimoAño.setText("VENTAS DE ULTIMO AÑO");
-        btnVentasUltimoAño.addActionListener(new java.awt.event.ActionListener() {
-            public void actionPerformed(java.awt.event.ActionEvent evt) {
-                btnVentasUltimoAñoActionPerformed(evt);
             }
         });
 
@@ -92,45 +78,32 @@ public class GestionVentas extends javax.swing.JFrame {
         jPanel1Layout.setHorizontalGroup(
             jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
             .addGroup(jPanel1Layout.createSequentialGroup()
-                .addGap(163, 163, 163)
-                .addComponent(btnVenta)
+                .addContainerGap()
+                .addComponent(btnVolver, javax.swing.GroupLayout.PREFERRED_SIZE, 100, javax.swing.GroupLayout.PREFERRED_SIZE)
+                .addContainerGap(javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE))
+            .addGroup(jPanel1Layout.createSequentialGroup()
+                .addGap(80, 80, 80)
+                .addComponent(jLabel1)
                 .addGap(0, 0, Short.MAX_VALUE))
             .addGroup(javax.swing.GroupLayout.Alignment.TRAILING, jPanel1Layout.createSequentialGroup()
-                .addContainerGap(57, Short.MAX_VALUE)
-                .addComponent(jLabel1)
-                .addGap(43, 43, 43))
-            .addGroup(jPanel1Layout.createSequentialGroup()
+                .addContainerGap(124, Short.MAX_VALUE)
                 .addGroup(jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-                    .addGroup(jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.TRAILING)
-                        .addComponent(btnVentasUltimoAño)
-                        .addGroup(jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-                            .addGroup(jPanel1Layout.createSequentialGroup()
-                                .addGap(113, 113, 113)
-                                .addComponent(btnVentasUltimoMes))
-                            .addGroup(jPanel1Layout.createSequentialGroup()
-                                .addGap(136, 136, 136)
-                                .addComponent(btnNotaVenta))))
-                    .addGroup(jPanel1Layout.createSequentialGroup()
-                        .addContainerGap()
-                        .addComponent(btnVolver, javax.swing.GroupLayout.PREFERRED_SIZE, 100, javax.swing.GroupLayout.PREFERRED_SIZE)))
-                .addContainerGap(javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE))
+                    .addComponent(btnVentasUltimoMes, javax.swing.GroupLayout.PREFERRED_SIZE, 180, javax.swing.GroupLayout.PREFERRED_SIZE)
+                    .addComponent(btnVenta, javax.swing.GroupLayout.PREFERRED_SIZE, 180, javax.swing.GroupLayout.PREFERRED_SIZE))
+                .addGap(124, 124, 124))
         );
         jPanel1Layout.setVerticalGroup(
             jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
             .addGroup(jPanel1Layout.createSequentialGroup()
-                .addGap(17, 17, 17)
-                .addComponent(jLabel1)
-                .addGap(18, 18, 18)
-                .addComponent(btnVenta)
-                .addGap(28, 28, 28)
-                .addComponent(btnNotaVenta)
-                .addGap(32, 32, 32)
-                .addComponent(btnVentasUltimoMes)
-                .addGap(37, 37, 37)
-                .addComponent(btnVentasUltimoAño)
-                .addGap(18, 18, 18)
+                .addGap(30, 30, 30)
+                .addComponent(jLabel1, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
+                .addGap(54, 54, 54)
+                .addComponent(btnVenta, javax.swing.GroupLayout.PREFERRED_SIZE, 60, javax.swing.GroupLayout.PREFERRED_SIZE)
+                .addGap(47, 47, 47)
+                .addComponent(btnVentasUltimoMes, javax.swing.GroupLayout.PREFERRED_SIZE, 60, javax.swing.GroupLayout.PREFERRED_SIZE)
+                .addGap(67, 67, 67)
                 .addComponent(btnVolver, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
-                .addContainerGap(8, Short.MAX_VALUE))
+                .addContainerGap())
         );
 
         javax.swing.GroupLayout layout = new javax.swing.GroupLayout(getContentPane());
@@ -156,26 +129,12 @@ public class GestionVentas extends javax.swing.JFrame {
         this.dispose();
     }//GEN-LAST:event_btnVentasUltimoMesActionPerformed
 
-    private void btnVentasUltimoAñoActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_btnVentasUltimoAñoActionPerformed
-        VentaAnual venta = new VentaAnual();
-        venta.setVisible(true);
-        venta.setLocationRelativeTo(null);
-        this.dispose();
-    }//GEN-LAST:event_btnVentasUltimoAñoActionPerformed
-
     private void btnVentaActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_btnVentaActionPerformed
         RegistroVenta reg = new RegistroVenta();
         reg.setVisible(true);
         reg.setLocationRelativeTo(null);
         this.dispose();
     }//GEN-LAST:event_btnVentaActionPerformed
-
-    private void btnNotaVentaActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_btnNotaVentaActionPerformed
-        RegistroNotaVenta notav=new RegistroNotaVenta();
-        notav.setVisible(true);
-        notav.setLocationRelativeTo(null);
-        this.dispose();
-    }//GEN-LAST:event_btnNotaVentaActionPerformed
 
     private void btnVolverMouseEntered(java.awt.event.MouseEvent evt) {//GEN-FIRST:event_btnVolverMouseEntered
         btnVolver.setBackground(new java.awt.Color(120, 0, 120));
@@ -198,9 +157,7 @@ public class GestionVentas extends javax.swing.JFrame {
     }
 
     // Variables declaration - do not modify//GEN-BEGIN:variables
-    private javax.swing.JButton btnNotaVenta;
     private javax.swing.JButton btnVenta;
-    private javax.swing.JButton btnVentasUltimoAño;
     private javax.swing.JButton btnVentasUltimoMes;
     private javax.swing.JButton btnVolver;
     private javax.swing.JLabel jLabel1;
