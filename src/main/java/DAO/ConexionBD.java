36a4423f92db8d55099d214f54f707b2c3b56c78
Andrle <andrelageu0607@gmail.com>
2024-12-01 17:55:44 -0500

    Conexión con base de datos en la nube

--- src/main/java/DAO/ConexionBD.java
+++ src/main/java/DAO/ConexionBD.java
@@ -7,9 +7,9 @@ import java.sql.SQLException;
 public class ConexionBD {
 
     public static Connection getConexion() throws SQLException {
-        String url = "jdbc:mysql://localhost:3306/bdesdiel";
-        String usuario = "root";
-        String contraseña = "";
+        String url = "jdbc:mysql://u44okiaiwbjavwj7:xXaZu81WKNomZyX6pKOo@b9cbymoe3xtutiwj1wwj-mysql.services.clever-cloud.com:3306/b9cbymoe3xtutiwj1wwj";
+        String usuario = "u44okiaiwbjavwj7";
+        String contraseña = "xXaZu81WKNomZyX6pKOo";
         return DriverManager.getConnection(url, usuario, contraseña);
     }
 }
